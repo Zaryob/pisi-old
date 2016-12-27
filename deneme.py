f9fec6f5f1be4410b04dcaf3041508fd3b211e2b
Nipalensisa <nipalensisaquila@gmail.com>
2016-12-27 14:54:35 +0200

    make changes

--- deneme.py
+++ deneme.py
@@ -1,3 +1,9 @@
 import sys
 sys.path.append(r'/home/pi/AquilaNipalensis/package-manager/')
+import pisi
+import pisi.actionsapi
+import pisi.cli
 import pisi.db
+import pisi.operations
+import pisi.pxml
+import pisi.scenarioapi
